17e25d89925486974f788eb691f080cc8875439c
sl1ked <viktor.butenko.00@gmail.com>
2020-11-19 14:10:40 +0300

    Теперь все работает, с распознаванием сенсорных устройств

--- script/sleep.js
+++ script/sleep.js
@@ -2,7 +2,7 @@ let button_sleep_left = document.getElementsByClassName(`scroll_left`)[0];
 let button_sleep_right = document.getElementsByClassName(`scroll_right`)[0];
 let conntent_block_width = document.getElementsByClassName(`popular__item`)[0].offsetWidth
 let popular = document.getElementsByClassName(`popular`)[0];
-const isTouch = (('ontouchstart' in window) || (navigator.msMaxTouchPoints > 0));
+const isTouch = (('ontouchstart' in window) || (navigator.msMaxTouchPoints > 0)); // проверка сенсорный ли экран устройства
 let re_sleep = () => {
     (popular.scrollLeft <= conntent_block_width / 2) ? button_sleep_left.style.display = `none`: button_sleep_left.style.display = `block`;
     (popular.scrollWidth - popular.offsetWidth < popular.scrollLeft + conntent_block_width / 2) ? button_sleep_right.style.display = `none`: button_sleep_right.style.display = `block`;
